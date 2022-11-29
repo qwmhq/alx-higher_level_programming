f0da193cc5a7fc7cfff12d59fa3c6fb6a93b1ed4
Quwam Ogunlaja <quwam32@gmail.com>
2022-11-29 03:30:21 +0100

    task 10 - linked list cycle

---
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -0,0 +1,23 @@
+#include "lists.h"
+
+/**
+ * check_cycle - check if a linked list has a cycle in it
+ *
+ * @list: the linked list
+ *
+ * Return: 0 if there is no cycle, 1 if there is a cycle
+ */
+int check_cycle(listint_t *list)
+{
+	listint_t *slow = list;
+	listint_t *fast = list->next;
+
+	while (fast && fast->next)
+	{
+		if (slow == fast)
+			return (1);
+		slow = slow->next;
+		fast = fast->next->next;
+	}
+	return (0);
+}
