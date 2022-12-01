4cd531ef94732d4ccbf42d5dd8dd7ef9319f5e50
Quwam Ogunlaja <quwam32@gmail.com>
2022-12-01 17:02:41 +0000

    corrected task 2

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,7 +3,7 @@ if __name__ == "__main__":
     import sys
     argc = len(sys.argv)
     print("{} argument{}{}".format(argc - 1,
-                                   '' if argc == 1 else 's',
-                                   '.' if argc < 1 else ':'))
+                                   '' if argc == 2 else 's',
+                                   '.' if argc < 2 else ':'))
     for i in range(1, argc):
         print("{}: {}".format(i, sys.argv[i]))
