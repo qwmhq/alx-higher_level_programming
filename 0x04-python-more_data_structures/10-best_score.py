036b6c34ff4b4cc98d98a6782ca94e4e49924f48
Quwam Ogunlaja <quwam32@gmail.com>
2022-12-07 16:32:34 +0000

    tasks 7 - 11

---
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+def best_score(a_dictionary):
+    if a_dictionary is None:
+        return None
+    best = list(a_dictionary)[0]
+    for k, v in a_dictionary.items():
+        if v > a_dictionary[best]:
+            best = k
+    return best
