82c7275258900edff9e7b94bcc9874355c932c70
Quwam Ogunlaja <quwam32@gmail.com>
2022-12-07 21:52:14 +0000

    task 12

---
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+def roman_to_int(roman_string):
+    numerals_key = {'I': 1, 'V': 5, 'X': 10,
+                    'L': 50, 'C': 100, 'D': 500, 'M': 1000}
+    sub = {'I': "VX", 'X': "LC", 'C': "DM"}
+    value = 0
+    for i, v in enumerate(roman_string):
+        if (i + 1 < len(roman_string) and v in sub and
+                roman_string[i + 1] in sub[v]):
+            value -= numerals_key[v]
+        else:
+            value += numerals_key[v]
+    return value
