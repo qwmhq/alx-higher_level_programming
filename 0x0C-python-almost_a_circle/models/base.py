746825348babb7fc7e682630e8a4eeeace73d134
Quwam Ogunlaja <quwam32@gmail.com>
2023-01-18 17:10:56 +0100

    from_json_string method

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -21,3 +21,10 @@ class Base():
         if list_dictionaries is None or len(list_dictionaries) == 0:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @staticmethod
+    def from_json_string(json_string):
+        """ get the list of the JSON string representation """
+        if json_string is None or json_string == "":
+            return []
+        return json.loads(json_string)
