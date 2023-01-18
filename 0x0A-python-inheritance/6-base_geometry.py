ced689eb714aadccf81e390fb0f3a32d6a14f425
Quwam Ogunlaja <quwam32@gmail.com>
2023-01-18 20:03:42 +0100

    area method for BaseGeometry class

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+""" 6-base_geometry """
+
+
+class BaseGeometry():
+    """ BaseGeometry class """
+
+    def area(self):
+        """ area method """
+        raise Exception("area() is not implemented")
