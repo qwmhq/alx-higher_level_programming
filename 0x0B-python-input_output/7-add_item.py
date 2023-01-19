0ac8899eedc3e9c3bd6d693f3793273ec44e1fcc
Quwam Ogunlaja <quwam32@gmail.com>
2023-01-19 11:13:27 +0100

    7-add_item.py script

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+""" 7-add_item """
+import sys
+save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
+load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+
+
+try:
+    args = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    args = []
+args += sys.argv[1:]
+save_to_json_file(args, "add_item.json")
