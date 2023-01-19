fb74caf1b81003e4ba66d6ecf93f2b9319da0e51
Quwam Ogunlaja <quwam32@gmail.com>
2023-01-20 00:36:48 +0100

    task 11 - reload_from_json

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,39 @@
+#!/usr/bin/python3
+""" 11-student """
+
+
+class Student():
+    """ Student class """
+
+    def __init__(self, first_name, last_name, age):
+        """
+        initialize a Student object
+
+        Args:
+            first_name (str): student's first name
+            last_name (str): student's last name
+            age (int): student's age
+        """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ retrieve the dictionary representation of a Student object """
+        if type(attrs) is list and all([type(x) is str for x in attrs]):
+            dct = {}
+            for attr in attrs:
+                if attr in self.__dict__.keys():
+                    dct[attr] = self.__dict__[attr]
+            return dct
+        return self.__dict__
+
+    def reload_from_json(self, json):
+        """
+        replace all attributes a Student Instance
+
+        Args:
+            json (dict): dictionary containing attributes to replace
+        """
+        for key, value in json.items():
+            setattr(self, key, value)
