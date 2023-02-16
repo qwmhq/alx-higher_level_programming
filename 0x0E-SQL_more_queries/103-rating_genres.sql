656e8186a616c3384a73bb23c7b0cad145bf2b89
Quwam Ogunlaja <quwam32@gmail.com>
2023-02-16 06:02:06 +0100

    task 20

---
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -0,0 +1,13 @@
+-- list all genres in the database by their rating
+-- display: tv_genres.name - rating sum
+-- order by rating (descending)
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
+FROM tv_genres
+INNER JOIN tv_show_genres
+ON tv_genres.id = tv_show_genres.genre_id
+INNER JOIN tv_shows
+ON tv_shows.id = tv_show_genres.show_id
+INNER JOIN tv_show_ratings
+ON tv_shows.id = tv_show_ratings.show_id
+GROUP BY tv_genres.name
+ORDER BY rating DESC;
