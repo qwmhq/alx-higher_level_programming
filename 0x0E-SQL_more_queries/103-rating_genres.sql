d191fab73dd79fa0d13422b33b3c870aac9739a0
Quwam Ogunlaja <quwam32@gmail.com>
2023-02-16 06:05:07 +0100

    corrected task 20

--- 0x0E-SQL_more_queries/103-rating_genres.sql
+++ 0x0E-SQL_more_queries/103-rating_genres.sql
@@ -1,7 +1,7 @@
 -- list all genres in the database by their rating
 -- display: tv_genres.name - rating sum
 -- order by rating (descending)
-SELECT tv_genres.name, SUM(tv_show_ratings.rate) as rating
+SELECT tv_genres.name, SUM(tv_show_ratings.rate) AS rating
 FROM tv_genres
 INNER JOIN tv_show_genres
 ON tv_genres.id = tv_show_genres.genre_id
