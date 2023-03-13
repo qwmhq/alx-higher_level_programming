977f38e3e9cd31b0854de7cedee795a3adb433f4
Quwam Ogunlaja <quwam32@gmail.com>
2023-03-13 22:15:23 +0100

    updated 11-second_biggest

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -1,3 +1,8 @@
 #!/usr/bin/node
 const args = process.argv.slice(2);
-console.log(args.map(Number).sort((a, b) => a - b)[1]);
+const argc = args.length;
+if (argc < 2) {
+  console.log(0);
+} else {
+  console.log(args.map(Number).sort((a, b) => b - a)[1]);
+}
