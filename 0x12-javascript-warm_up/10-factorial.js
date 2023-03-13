3e34316b128289a86c2009a734ffa1222e25176f
Quwam Ogunlaja <quwam32@gmail.com>
2023-03-13 22:15:06 +0100

    updated 10-factorial

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -6,7 +6,4 @@ function factorial (x) {
   return x * factorial(x - 1);
 }
 const num = Number(process.argv[2]);
-if (isNaN(num)) {
-  console.log('argument must be a number');
-}
 console.log(factorial(num));
