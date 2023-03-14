39a93bc9f3b9c33d7b117cc1c20b25d13ad5e4eb
Quwam Ogunlaja <43578101+qwmhq@users.noreply.github.com>
2023-03-14 13:17:49 +0100

    Update 10-converter.js

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,11 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
-  function convert (num) {
-    const d = '0123456789abcdefhijklmnopqrstuvwxyz';
-    if (!Math.floor(num / base)) {
-      return d[num % base];
-    }
-    return convert(Math.floor(num / base)) + d[num % base];
-  }
-  return convert;
+  return function (num) {
+    return num.toString(base);
+  };
 };
