c6fc7d71978fceb1863caf836c8921c7ae94d216
Quwam Ogunlaja <quwam32@gmail.com>
2023-03-17 11:25:42 +0100

    corrected 1-filter_states

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -12,14 +12,14 @@ if __name__ == '__main__':
                          port=3306,
                          user=sys.argv[1],
                          passwd=sys.argv[2],
-                         db=sys.argv[3])
+                         db=sys.argv[3],
+                         charset="utf8")
     cur = db.cursor()
     cur.execute("SELECT *\
                  FROM states\
-                 WHERE name REGEXP '^N'\
+                 WHERE name LIKE BINARY 'N%'\
                  ORDER BY id ASC;")
     for row in cur.fetchall():
         print(row)
-
     cur.close()
     db.close()
