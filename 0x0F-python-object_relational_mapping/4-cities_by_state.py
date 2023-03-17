254412e177738190caf669cb1eb4d1e1fb00dd62
Quwam Ogunlaja <quwam32@gmail.com>
2023-03-17 11:54:38 +0100

    4-cities_by_state

--- 0x0F-python-object_relational_mapping/4-cities_by_state.py
+++ 0x0F-python-object_relational_mapping/4-cities_by_state.py
@@ -17,8 +17,8 @@ if __name__ == '__main__':
                          db=database)
     cur = db.cursor()
     cur.execute("SELECT cities.id, cities.name, states.name\
-                FROM states\
-                LEFT JOIN cities\
+                FROM cities\
+                LEFT JOIN states\
                 ON cities.state_id = states.id\
                 ORDER BY cities.id ASC;")
     for row in cur.fetchall():
